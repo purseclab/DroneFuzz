cebb475f0489b6a65dcb9c9ae292ad3eec529bd4
silipwn <contact@as-hw.in>
2024-05-23 22:17:01 +0000

    Add support for Rangefinder

--- ArduPilot/open_simulator.py
+++ ArduPilot/open_simulator.py
@@ -14,7 +14,7 @@ print(os.getcwd())
 if ARDUPILOT_HOME is None:
     raise Exception("ARDUPILOT_HOME environment variable is not set!")
 
-c = ARDUPILOT_HOME + "Tools/autotest/sim_vehicle.py -v ArduCopter --console --map -w"
+c = ARDUPILOT_HOME + "Tools/autotest/sim_vehicle.py -v ArduCopter -w --out=udp:127.0.0.1:1337"
 # c = '~/ardupilot_pgfuzz/Tools/autotest/sim_vehicle.py -v ArduCopter --console --map -w'
 
 # handle = Popen(c, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
