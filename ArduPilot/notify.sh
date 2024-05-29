7122a899b32621e75d8bc49352fd6b5ae0ae02e0
silipwn [Ashwin] <nambiar1@purdue.edu>
2024-05-29 14:54:10 -0400

    Add hostname to distinguish the hosts

--- ArduPilot/notify.sh
+++ ArduPilot/notify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 log=$(tail -n 7 $PGFUZZ_HOME/ArduPilot/fuzzing.log)
-curl -s -X POST -d chat_id=$CHAT_ID -d text="Fuzzer exited with following: $log" "https://api.telegram.org/bot$TOKEN/sendMessage" > /dev/null
+curl -s -X POST -d chat_id=$CHAT_ID -d text="Fuzzer on $(hostname) exited with following: $log" "https://api.telegram.org/bot$TOKEN/sendMessage" > /dev/null
