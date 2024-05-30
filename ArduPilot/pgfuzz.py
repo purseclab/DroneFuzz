3b9882a59d213da18d89d9150d2fd141700c145c
silipwn <contact@as-hw.in>
2024-05-30 17:56:22 -0400

    Add support for spawning things inside TMUX

--- ArduPilot/pgfuzz.py
+++ ArduPilot/pgfuzz.py
@@ -1,6 +1,72 @@
 import time
-from subprocess import *
+import datetime
+import signal
+import psutil
+import subprocess
+# from subprocess import *
+from pdb import set_trace as bp
 import os
+
+# List to keep track of child processes
+child_processes = []
+
+
+def sigint_handler(signum, frame):
+    print("SIGINT received, terminating child processes...")
+    # TODO Actually kill the processes?
+    exit(0)
+
+
+def spawn_tmux_window(session_name='pgfuzz++', window_name='', command=''):
+    """
+    Spawns a new tmux window in the specified session and runs an optional command.
+
+    :param session_name: Name of the tmux session to create the window in.
+    :param window_name: Name of the new window.
+    :param command: Command to run in the new window.
+    """
+    try:
+        tmux_env = os.getenv('TMUX')
+        if tmux_env:
+            # The TMUX variable is set to a value like "/tmp/tmux-1000/default,1234,0"
+            # We need to get the session name which is usually part of the path
+            result = subprocess.check_output(
+                ['tmux', 'display-message', '-p', '#S'])
+            session_name = result.strip()
+        else:
+            # Check if the session exists
+            result = subprocess.Popen(
+                ['tmux', 'has-session', '-t', session_name],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+            result.communicate()
+            if result.returncode != 0:
+                # Create the session if it doesn't exist
+                subprocess.call(
+                    ['tmux', 'new-session', '-d', '-s', session_name])
+                print("Created new session: {0}".format(session_name))
+
+        # Create a new window in the specified session
+        new_window_command = ['tmux', 'new-window', '-t', session_name]
+        if window_name:
+            new_window_command.extend(['-n', window_name])
+        subprocess.call(new_window_command)
+
+        # Run the command in the new window if provided
+        if command:
+            target = "{0}:{1}".format(
+                session_name, window_name) if window_name else session_name
+            subprocess.call(
+                ['tmux', 'send-keys', '-t', target, command, 'C-m'])
+
+        print("New window created in session {0}.".format(session_name))
+
+    except Exception as e:
+        print("An error occurred: {0}".format(e))
+
+
+# except Exception as e:
+#     print(f"An error occurred: {e}")
 
 PGFUZZ_HOME = os.getenv("PGFUZZ_HOME")
 
@@ -14,34 +80,30 @@ if ARDUPILOT_HOME is None:
 
 open("restart.txt", "w").close()
 
-c = (
-    "gnome-terminal -- bash -c 'python2 "
-    + PGFUZZ_HOME
-    + "ArduPilot/open_simulator.py; exec bash'"
-)
-print(c)
-handle = Popen(c, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
+# Files to open
+working_dir = PGFUZZ_HOME + "ArduPilot/"
+open_simulator = working_dir + "open_simulator.py"
+setup_sh = working_dir + "setup.sh"
+fuzzing_py = working_dir + "fuzzing.py"
+
+# Register the SIGINT handler
+signal.signal(signal.SIGINT, sigint_handler)
+
+cmd = 'source ' + setup_sh + '; python2 ' + open_simulator + '; exit'
+spawn_tmux_window(window_name='sitl', command=cmd)
+
 
 time.sleep(90)
-c = (
-    "gnome-terminal -- bash -c 'python2 "
-    + PGFUZZ_HOME
-    + "ArduPilot/fuzzing.py | tee fuzzing.log ; exec bash'"
-)
-print(c)
-handle = Popen(c, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
+cmd = 'source ' + setup_sh + '; python2 ' + fuzzing_py + ' | tee fuzzing.log'
+spawn_tmux_window(window_name='fuzzing', command=cmd)
 
 while True:
     time.sleep(1)
-
     f = open("restart.txt", "r")
-
     if f.read() == "restart":
         f.close()
         open("restart.txt", "w").close()
-        c = (
-            "gnome-terminal -- bash -c 'python2 "
-            + PGFUZZ_HOME
-            + "ArduPilot/open_simulator.py; exec bash'"
-        )
-        handle = Popen(c, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
+        cmd = 'source ' + setup_sh + '; python2 ' + open_simulator + '; exit'
+        # Get the current datetime in Unix seconds
+        window_name = 'sitl-' + str(int(time.time()))
+        spawn_tmux_window(window_name=window_name, command=cmd)
