1ee49b0cb3606debe6f52d7acfd313cd63b273dc
Ashwin Nambiar <nambiar1@purdue.edu>
2024-06-13 11:51:52 -0400

    Prox sensor script mods
    1. Ensure that we are able to run the mission for longer

--- bug-reproduction/prox_avoid_sensor/sensor_emulate.py
+++ bug-reproduction/prox_avoid_sensor/sensor_emulate.py
@@ -6,21 +6,28 @@ from pymavlink import mavutil
 import os
 
 sys.path.append("/usr/local/lib/")
-import time
 
 # Connect to the UAV's MAVLink interface over TCP
-conn = mavutil.mavlink_connection('localhost:14551',source_system=1,source_component=93,baud=921600)
+conn = mavutil.mavlink_connection(
+    "localhost:14551", source_system=1, source_component=93, baud=921600
+)
 
-os.environ['MAVLINK20'] = '1'
-os.environ['MAVLINK_DIALECT'] = 'ardupilotmega'
+print("Connected to Mavlink interface")
+
+os.environ["MAVLINK20"] = "1"
+os.environ["MAVLINK_DIALECT"] = "ardupilotmega"
 
 # get time in correct format
-start_time =  int(round(time.time() * 1000))
-current_milli_time = lambda: int(round(time.time() * 1000) - start_time)
-DEPTH_RANGE = [0.3, 12] # depth range, to be changed as per requirements
-MAX_DEPTH = 9999 # arbitrary large number
+start_time = int(round(time.time() * 1000))
+# current_milli_time = lambda: int(round(time.time() * 1000) - start_time)
+DEPTH_RANGE = [0.3, 12]  # depth range, to be changed as per requirements
+MAX_DEPTH = 9999  # arbitrary large number
 
 obstacle_bring = False
+
+
+def current_milli_time():
+    return int(round(time.time() * 1000) - start_time)
 
 
 def random_normal_value():
@@ -28,64 +35,70 @@ def random_normal_value():
     # Uniformly send values
     x = y = z = [0.00] * 9
     for i in range(9):
-        x[i] = random.uniform(-full_range,full_range)
-        y[i] = random.uniform(-full_range,full_range)
-        z[i] = random.uniform(-full_range,full_range)
-    return x,y,z
+        x[i] = random.uniform(-full_range, full_range)
+        y[i] = random.uniform(-full_range, full_range)
+        z[i] = random.uniform(-full_range, full_range)
+    return x, y, z
+
 
 def random_attack_value():
-    half_range = DEPTH_RANGE[1]/2
+    half_range = DEPTH_RANGE[1] / 2
 
     # Uniformly send values
     x = y = z = [0.00] * 9
     for i in range(9):
-        x[i] = random.uniform(-half_range/2,0)
-        y[i] = random.uniform(0,half_range/2)
-        z[i] = random.uniform(0,half_range)
-    return x,y,z
+        x[i] = random.uniform(half_range / 8, 5)
+        y[i] = random.uniform(5, half_range / 8)
+        z[i] = DEPTH_RANGE[1]
+    return x, y, z
+
 
 # Function to send a simulated sensor input packet
 def send_sensor_input(value):
     time = current_milli_time()
     for i in range(9):
         msg = mavlink2.MAVLink_obstacle_distance_3d_message(
-        # conn.mav.obstacle_distance_3d_send(
-            time,    # us Timestamp (UNIX time or time since system boot)
-            0,       # not implemented in ArduPilot            
-            12,      # Set the frame to MAV_FRAME_BODY_FRD
-            65535,   # unknown ID of the object. We are not really detecting the type of obstacle           
-            float(value[0][i]),	   # X in NEU body frame 
-            float(value[1][i]),     # Y in NEU body frame  
-            float(value[2][i]),	   # Z in NEU body frame  
-            float(DEPTH_RANGE[0]), # min range of sensor
-            float(DEPTH_RANGE[1])  # max range of sensor
+            # conn.mav.obstacle_distance_3d_send(
+            time,  # us Timestamp (UNIX time or time since system boot)
+            0,  # not implemented in ArduPilot
+            12,  # Set the frame to MAV_FRAME_BODY_FRD
+            65535,  # unknown ID of the object. We are not really detecting the type of obstacle
+            float(value[0][i]),  # X in NEU body frame
+            float(value[1][i]),  # Y in NEU body frame
+            float(value[2][i]),  # Z in NEU body frame
+            float(DEPTH_RANGE[0]),  # min range of sensor
+            float(DEPTH_RANGE[1]),  # max range of sensor
         )
         conn.mav.send(msg)
+
 
 # Normal scenario
 def normal_exec():
     itr = 0
     hz = 25
-    while(itr <= hz):
+    while itr <= hz:
         send_sensor_input(random_normal_value())
-        time.sleep(1/hz)
+        time.sleep(1 / hz)
         itr += 1
+
 
 # Attack mode function
 def obstacle_exec():
     itr = 0
     hz = 10
-    while(itr <= hz):
+    while itr <= hz:
         send_sensor_input(random_attack_value())
-        time.sleep(1/hz)
+        time.sleep(1 / hz)
         itr += 1
+
 
 # Function to check if the copter is in the desired state
 def is_copter_ready():
-    msg = conn.recv_match(type='HEARTBEAT', blocking=True)
+    msg = conn.recv_match(type="HEARTBEAT", blocking=True)
     is_armed = bool(msg.base_mode & mavutil.mavlink.MAV_MODE_FLAG_SAFETY_ARMED)
     is_loiter_mode = msg.custom_mode == mavutil.mavlink.COPTER_MODE_LOITER
     return is_armed and is_loiter_mode
+
 
 # Main loop
 while True:
