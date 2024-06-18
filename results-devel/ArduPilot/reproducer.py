101e030a27bac798be035a3ce8dba44299419b67
Ashwin Nambiar <nambiar1@purdue.edu>
2024-06-18 12:38:20 -0400

    Renaming results

--- results-devel/ArduPilot/reproducer.py
+++ results-devel/ArduPilot/reproducer.py
@@ -6,5 +6,6 @@ import pymavlink.dialects.v20.all as dialect
 sensor_conn = mavutil.mavlink_connection("udp:127.0.0.1:1337")
 sensor_conn.wait_heartbeat()
 
+# TODO
 gcs_conn = mavutil.mavlink_connection("udp:127.0.0.1:14551")
 gcs_conn.wait_heartbeat()
