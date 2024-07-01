911f66b9259b791568b6deaf6202d7f47714d631
Ashwin Nambiar <nambiar1@purdue.edu>
2024-07-01 12:36:19 -0400

    Fix formatting

--- ArduPilot/open_simulator.py
+++ ArduPilot/open_simulator.py
@@ -19,6 +19,7 @@ c = (
     ARDUPILOT_HOME
     + "Tools/autotest/sim_vehicle.py -v ArduCopter --out=udp:127.0.0.1:1337 -D --gdb"
 )
+# "Tools/autotest/sim_vehicle.py -v ArduCopter --out=udp:127.0.0.1:1337 -D --gdb --out=tcpin:127.0.0.1:14560"
 # c = '~/ardupilot_pgfuzz/Tools/autotest/sim_vehicle.py -v ArduCopter --console --map -w'
 
 # handle = Popen(c, stdin=PIPE, stderr=PIPE, stdout=PIPE, shell=True)
