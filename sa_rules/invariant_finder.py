50c88b86305c9f0d6b22e2000ed951a602acf594
silipwn <contact@as-hw.in>
2024-12-03 17:12:43 -0500

    misc: Restructure code

--- sa_rules/invariant_finder.py
+++ sa_rules/invariant_finder.py
@@ -27,7 +27,7 @@ ch = logging.StreamHandler()
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
-def find_consec_invariants(msg_dict: dict,threshold: float):
+def find_consec_invariants(msg_dict: dict,threshold: float) -> dict:
     """
     Find invariants in a dictionary of messages, based on where consec values 
     don't change more than the threshold
@@ -53,12 +53,28 @@ def find_consec_invariants(msg_dict: dict,threshold: float):
                     invariants[msg].append(key)
                 else:
                     invariants[msg] = [key]
-    print("Invariants found:")
-    value = json.dumps(invariants, indent=4)
-    print(value)
-    exit(0)
+    return invariants
+    
+def parse_tlog(tlog: str) -> dict:
+    # Connect to the TLog file
+    mavutil_log = mavutil.mavlink_connection(args.tlog)
+    # Extract all the messages into a internal dictionary
+    while True:
+        msg = mavutil_log.recv_msg()
+        if msg is None:
+            break
+        msg = msg.to_dict()
+        if msg['mavpackettype'] == 'BAD_DATA':
+            logger.warning("Bad data found in Tlog")
+            continue
+        # Group messages by their type
+        if msg['mavpackettype'] in msg_dict:
+            msg_dict[msg['mavpackettype']].append(msg)
+        else:
+            msg_dict[msg['mavpackettype']] = [msg]
+    return msg_dict
 
-def find_std_dev(msg_dict: dict,threshold: float):
+def find_std_dev(msg_dict: dict,threshold: float) -> dict:
     """
     Find invariants in a dictionary of messages, based on the standard deviation of the values
     """
@@ -71,79 +87,76 @@ def find_std_dev(msg_dict: dict,threshold: float):
                 # Try to convert the value to a float/int if possible, else generate a warning
                 try:
                     data[i][key] = float(data[i][key])
-                    data[i-1][key] = float(data[i-1][key])
                 except: 
-                    print(f"Warning: {key} is not a float/int in {msg}")
+                    logger.warning(f"{key} is not a float/int in {msg}")
                     invariant = False
                     break
-                values.append(data[i][key] - data[i-1][key])
+                values.append(data[i][key])
             if invariant:
                 std_dev = np.std(values)
-                if std_dev < threshold:
+                logger.debug("std_dev {} for {}".format(std_dev,key))
+                if std_dev == 0.0:
                     if msg in invariants:
                         invariants[msg].append(key)
                     else:
                         invariants[msg] = [key]
-    print("Invariants found:")
-    value = json.dumps(invariants, indent=4)
-    print(value)
-    exit(0)
+    return invariants
+    
 
 
 parser = argparse.ArgumentParser(description='Find invariants in a TLog file')
 parser.add_argument('--tlog', type=str, help='TLog file to analyze')
-parser.add_argument('--json', type=str, help='JSON file to load')
+parser.add_argument('--folder', type=str, help='Folder to load Tlogs from to merge')
 parser.add_argument('--threshold', type=float, help='Threshold for invariants (Default: 0.9)', default=0.9)
 parser.add_argument('--mode',choices=['consec','std_dev'],help='Mode to run in (Default: std_dev)',default='std_dev')
+parser.add_argument('--verbose', help='Enable verbose logging', action='store_true')
 args = parser.parse_args()
 
-print("Checking for invariants with threshold ", args.threshold)
+logger.info(f"Checking for invariants with threshold {args.threshold}")
 
+if args.verbose:
+    logger.setLevel(logging.DEBUG)
+    logger.debug("Verbose logging enabled")
 
 
 # Check if tlog xor json is provided
-if args.json is None and args.tlog is None:
-    print("Please provide a JSON/Tlog file to load")
+if args.tlog is None and args.folder is None:
+    logger.critical("Please provide a JSON/Tlog file to load")
     exit(1)
 
 msg_dict = {}
-if args.tlog:
-    # Connect to the TLog file
-    mavutil_log = mavutil.mavlink_connection(args.tlog)
-    # Extract all the messages into a internal dictionary
-    while True:
-        msg = mavutil_log.recv_msg()
-        if msg is None:
-            break
-        msg = msg.to_dict()
-        if msg['mavpackettype'] == 'BAD_DATA':
-            print("Warning: Bad data found in Tlog")
-            exit(1)
-        # Group messages by their type
-        if msg['mavpackettype'] in msg_dict:
-            msg_dict[msg['mavpackettype']].append(msg)
-        else:
-            msg_dict[msg['mavpackettype']] = [msg]
-    # Now track the invariants
-        
 
-if args.json:
-    # Load the JSON file
-    with open(args.json, 'r') as f:
-        for line in f:
-            # Data looks like this 
-            # {'meta': {'type': 'HEARTBEAT', 'timestamp': 1730766586.291339}, 'data': {'type': 6, 'autopilot': 8, 'base_mode': 0, 'custom_mode': 0, 'system_status': 0, 'mavlink_version': 3}}
-            # We need to extract the 'meta' and 'data' fields
-            line = json.loads(line)
-            msg = line['meta']['type']
-            if msg in msg_dict:
-                msg_dict[msg].append(line['data'])
-            else:
-                msg_dict[msg] = [line['data']]
-    
+if args.tlog:
+    msg_dict = parse_tlog(args.tlog)
+
+elif args.folder:
+    # Load all the TLog files from the folder
+    merge_dict = {}
+    for file in os.listdir(args.folder):
+        if file.endswith(".tlog"):
+            logger.info(f"Loading {file}")
+            msg_dict = parse_tlog(args.folder + "/" + file)
+            logger.critical("Merging not implemented yet")
+            exit(1)
+            # Now track the invariants
+            # if args.mode == 'std_dev':
+            #     invariants = find_std_dev(msg_dict,args.threshold)
+            # else:
+            #     invariants = find_consec_invariants(msg_dict,args.threshold)
+            # # Merge the invariants
+            # for key in invariants:
+            #     if key in merge_dict:
+            #         merge_dict[key] += invariants[key]
+            #     else:
+            #         merge_dict[key] = invariants[key]
 
 invariants = {}
 if args.mode == 'std_dev':
-    find_std_dev(msg_dict,args.threshold)
+    invariants = find_std_dev(msg_dict,args.threshold)
 else:
-    find_consec_invariants(msg_dict,args.threshold)
+    invariants = find_consec_invariants(msg_dict,args.threshold)
+
+# Save the invariants into a JSON file
+with open('invariants.json', 'w') as f:
+    json.dump(invariants, f, indent=4)
+    logger.info("Invariants saved to invariants.json")    
