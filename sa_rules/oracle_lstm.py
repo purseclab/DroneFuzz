2832bf3bd58d7cc71817de2a62f27b86794e5cfd
silipwn <contact@as-hw.in>
2025-02-27 13:48:58 -0500

    feat: Add misc features to oracle

--- sa_rules/oracle_lstm.py
+++ sa_rules/oracle_lstm.py
@@ -4,75 +4,84 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import roc_auc_score
 from tensorflow.keras.models import Model, Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout, Input, RepeatVector, TimeDistributed
 import matplotlib.pyplot as plt
 
 # --- Step 1: Load CSV files from a folder and select desired columns ---
-input_folder = 'csvs/' # Replace with your folder path
+# input_folder = '/mnt/oldhd/Data/PGFUZZ-data/ml_data/rfbug_stable' # Replace with your folder path
+input_folder = '/mnt/oldhd/Data/PGFUZZ-data/ml_data/gimbalyaw_stable' # Replace with your folder path
 rc_csv_files = glob.glob(os.path.join(input_folder, '*_RCOU.csv'))
-sim_csv_files = glob.glob(os.path.join(input_folder, '*_SIM.csv'))
+# sim_csv_files = glob.glob(os.path.join(input_folder, '*_SIM.csv'))
 
-anomaly_csv = 'csvs/anomaly/'
+anomaly_csv = input_folder + '/anomaly/'
 rc_anomaly_files = glob.glob(os.path.join(anomaly_csv, '*_RCOU.csv'))
-sim_anomaly_files = glob.glob(os.path.join(anomaly_csv, '*_SIM.csv'))
+# sim_anomaly_files = glob.glob(os.path.join(anomaly_csv, '*_SIM.csv'))
 
-fixed_csv = 'csvs/fixed/'
+fixed_csv = input_folder + '/fixed/'
 rc_fixed_files = glob.glob(os.path.join(fixed_csv, '*_RCOU.csv'))
-sim_fixed_files = glob.glob(os.path.join(fixed_csv, '*_SIM.csv'))
+# sim_fixed_files = glob.glob(os.path.join(fixed_csv, '*_SIM.csv'))
 
 # List of columns to extract (for example, two features and one target)
 selected_cols_rc = ['C1', 'C2', 'C3','C4']
-selected_cols_sim = ['Roll','Pitch','Yaw','Alt','Lat','Lng','Q1','Q2','Q3','Q4']
+# selected_cols_sim = ['Q1','Q2','Q3','Q4']
 
 combined_mapping_dict = {
     0: 'C1',
     1: 'C2',
     2: 'C3',
     3: 'C4',
-    4: 'Roll',
-    5: 'Pitch',
-    6: 'Yaw',
-    7: 'Alt',
-    8: 'Lat',
-    9: 'Lng',
-    10: 'Q1',
-    11: 'Q2',
-    12: 'Q3',
-    13: 'Q4',
+    4: 'Q1',
+    5: 'Q2',
+    6: 'Q3',
+    7: 'Q4',
+    8: 'Roll',
+    9: 'Pitch',
+    10: 'Yaw',
+    11: 'Alt',
+    12: 'Lat',
+    13: 'Lng',
 }
 
 # Read and concatenate CSV files
 df_list = [pd.read_csv(f)[selected_cols_rc] for f in rc_csv_files]
 data_rc = pd.concat(df_list, ignore_index=True)
 
-df_list = [pd.read_csv(f)[selected_cols_sim] for f in sim_csv_files]
-data_sim = pd.concat(df_list, ignore_index=True)
+# df_list = [pd.read_csv(f)[selected_cols_sim] for f in sim_csv_files]
+# data_sim = pd.concat(df_list, ignore_index=True)
 
 df_test = [pd.read_csv(f)[selected_cols_rc] for f in rc_anomaly_files]
 data_anomaly_rc = pd.concat(df_test, ignore_index=True)
 
-df_test = [pd.read_csv(f)[selected_cols_sim] for f in sim_anomaly_files]
-data_anomaly_sim = pd.concat(df_test, ignore_index=True)
+# df_test = [pd.read_csv(f)[selected_cols_sim] for f in sim_anomaly_files]
+# data_anomaly_sim = pd.concat(df_test, ignore_index=True)
 
 df_fixed = [pd.read_csv(f)[selected_cols_rc] for f in rc_fixed_files]
 data_fixed_rc = pd.concat(df_fixed, ignore_index=True)
 
-df_fixed = [pd.read_csv(f)[selected_cols_sim] for f in sim_fixed_files]
-data_fixed_sim = pd.concat(df_fixed, ignore_index=True)
+# df_fixed = [pd.read_csv(f)[selected_cols_sim] for f in sim_fixed_files]
+# data_fixed_sim = pd.concat(df_fixed, ignore_index=True)
 
 
 # Merge the two dataframes
-data = pd.concat([data_rc, data_sim], axis=1)
-data_test = pd.concat([data_anomaly_rc, data_anomaly_sim], axis=1)
-data_fixed = pd.concat([data_fixed_rc, data_fixed_sim], axis=1)
+# data = pd.concat([data_rc, data_sim], axis=1)
+# data_test = pd.concat([data_anomaly_rc, data_anomaly_sim], axis=1)
+# data_fixed = pd.concat([data_fixed_rc, data_fixed_sim], axis=1)
 
+# Convert the time series data into frequency domain
+# data= np.fft.fft(data_rc)
+# data_test = np.fft.fft(data_anomaly_rc)
+# data_fixed = np.fft.fft(data_fixed_rc)
+data = data_rc
+data_test = data_anomaly_rc
+data_fixed = data_fixed_rc
 # --- Step 2: Prepare features and target ---
 # Separate features and target column
-features = data[selected_cols_rc].values
-targets = data[selected_cols_sim].values
+features = data[selected_cols_rc].values 
+# targets = data[selected_cols_sim].values
 
-num_features = features.shape[1] + targets.shape[1]
+num_features = features.shape[1] # + targets.shape[1]
 # --- Step 2: Create Time-Series Sequences ---
 # For anomaly detection, we want to reconstruct the same sequence.
 
@@ -107,6 +116,8 @@ if os.path.isfile('autoencoder_model.keras'):
     from tensorflow.keras.models import load_model
     autoencoder = load_model('autoencoder_model.keras')
     print("Model loaded")
+    threshold = np.load('reconstruction_threshold.npy')
+    print(f"Loaded threshold for anomaly detection: {threshold}")
 else:
 # --- Step 4: Split into Training and Testing Sets ---
 # Ideally, the training data consists of only "normal" sequences.
@@ -116,36 +127,41 @@ else:
 # --- Step 5: Build an LSTM Autoencoder Model ---
 # Encoder
     inputs = Input(shape=(window_size, num_features))
-    encoded = LSTM(128, return_sequences=True)(inputs)
+    encoded = LSTM(128,activation='relu',return_sequences=True)(inputs)
     encoded = Dropout(0.2)(encoded)
-    encoded = LSTM(64, return_sequences=False)(encoded)
+    encoded = LSTM(64,activation='relu',return_sequences=False)(encoded)
 # Bottleneck representation
     bottleneck = RepeatVector(window_size)(encoded)
 # Decoder
-    decoded = LSTM(64, return_sequences=True)(bottleneck)
+    decoded = LSTM(64,activation='relu', return_sequences=True)(bottleneck)
     decoded = Dropout(0.2)(decoded)
-    decoded = LSTM(128, return_sequences=True)(decoded)
+    decoded = LSTM(128,activation='relu', return_sequences=True)(decoded)
 # TimeDistributed output layer to reconstruct original features at each timestep
     outputs = TimeDistributed(Dense(num_features))(decoded)
 
     autoencoder = Model(inputs, outputs)
 # TODO Explore if higher learning rate would improve
-    autoencoder.compile(optimizer='adam', loss='mse')
+    autoencoder.compile(optimizer='adam', loss='mae')
     autoencoder.summary()
 
 # --- Step 6: Train the Model ---
-    history = autoencoder.fit(X_train, X_train, 
-                            epochs=40, 
+    history = autoencoder.fit(X_train, X_train,
+                            epochs=25, 
                             batch_size=32, 
                             validation_data=(X_test, X_test))
 
 # --- Step 7: Compute Reconstruction Error ---
-# Predict on test set
+    # Predict on test set
     X_test_pred = autoencoder.predict(X_test)
-# Calculate MSE for each sequence (averaging over timesteps and features)
+    # Calculate MSE for each sequence (averaging over timesteps and features)
     reconstruction_errors = np.mean(np.power(X_test - X_test_pred, 2), axis=(1,2))
     print(f"Mean of reconstruction errors: {np.mean(reconstruction_errors)}")
+    # Threshold for anomaly detection (e.g., 95th percentile)
+    threshold = np.percentile(reconstruction_errors, 95)
+    print(f"Reconstruction error threshold for anomaly detection: {threshold}")
 
+
+    
 # Plot histogram of reconstruction errors
     plt.figure(figsize=(8,4))
     plt.hist(reconstruction_errors, bins=50)
@@ -162,6 +178,9 @@ else:
 
 # Save the model
     autoencoder.save('autoencoder_model.keras')
+    # Save the threshold
+    np.save('reconstruction_threshold.npy', threshold)
+    print("Model saved")
 
 # Plot each feature (or average over features)
     plt.figure(figsize=(10,6))
@@ -175,49 +194,66 @@ else:
 
 # Check with real anomaly data
 X_test_pred = autoencoder.predict(test_data_scaled)
-reconstruction_errors = np.mean(np.power(test_data_scaled - X_test_pred, 2), axis=(1,2))
-print(f"Mean of reconstruction errors: {np.mean(reconstruction_errors)}")
+reconstruction_errors = np.mean(np.power(test_data_scaled - X_test_pred, 2), axis=(1))
+
+# You can set a threshold (e.g., based on percentile or statistical properties)
+# Calculate anomalies for each feature seperately, and store into an array for plotting
+anomalies = np.zeros((test_data_scaled.shape[0], num_features), dtype=bool)
+for index in range(num_features):
+    feature_reconstruction_errors = np.mean(np.power(test_data_scaled[:, :, index] - X_test_pred[:, :, index], 2), axis=1)
+    threshold = np.percentile(feature_reconstruction_errors, 95)  # 95th percentile as threshold
+    anomalies[:, index] = feature_reconstruction_errors > threshold
+    print(f"Number of anomalies detected in feature {combined_mapping_dict[index]}: {np.sum(anomalies[:, index])} out of {len(reconstruction_errors)} samples, The current threshold is {threshold}")
+
+# anomalies = reconstruction_errors > threshold
+# print(f"Number of anomalies detected: {np.sum(anomalies)} out of {len(reconstruction_errors)} samples, The current threshold is {threshold}")
+# # Create a variable to store the index of the anomalies relative to the test_data
+# anomaly_indices = np.where(anomalies)[0]
+# Find the indices only where values less than the features are changed
+# Split the anomaly_indices into number of features
 
 # Plot the original and reconstructed sequence only per feature
-# Extract one feature for plotting
+# Create a subplot for each feature
+# Scatter plot the anomalies
+fig, ax = plt.subplots(num_features, 1)
 for index in range(num_features):
     original_feature = test_data_scaled[:, :, index]
-    reconstructed_feature = X_test_pred[:, :, index] 
-    plt.figure(figsize=(10, 6))
-    plt.plot(original_feature.mean(axis=1), label="Original (avg over timesteps)")
-    plt.plot(reconstructed_feature.mean(axis=1), label="Reconstructed (avg over timesteps)")
-    plt.xlabel("Sample")
-    plt.ylabel(f"Average Feature Value {combined_mapping_dict[index]}")
-    plt.title("Original vs. Reconstructed Feature")
-    plt.legend()
-    plt.show()
+    reconstructed_feature = X_test_pred[:, :, index]
+    ax[index].plot(original_feature.mean(axis=1), label="Original (avg over timesteps)")
+    ax[index].plot(reconstructed_feature.mean(axis=1), label="Reconstructed (avg over timesteps)")
+    ax[index].scatter(np.where(anomalies[:, index])[0], original_feature.mean(axis=1)[anomalies[:, index]], c='red', s=10)
+    ax[index].set_xlabel("Sample")
+    ax[index].set_ylabel(f"Average Feature Value {combined_mapping_dict[index]}")
+    ax[index].legend()
+plt.show()
+
 # --- Optional: Flag Anomalies ---
-# You can set a threshold (e.g., based on percentile or statistical properties)
-threshold = np.percentile(reconstruction_errors, 95)  # 95th percentile as threshold
-anomalies = reconstruction_errors > threshold
-print(f"Number of anomalies detected: {np.sum(anomalies)} out of {len(reconstruction_errors)} samples, The current threshold is {threshold}")
+
 
 # Now test with fixed data
 X_test_pred = autoencoder.predict(fixed_data_scaled)
-reconstruction_errors = np.mean(np.power(fixed_data_scaled - X_test_pred, 2), axis=(1,2))
+reconstruction_errors = np.mean(np.power(fixed_data_scaled - X_test_pred, 2), axis=(1))
 print(f"Mean of reconstruction errors: {np.mean(reconstruction_errors)}")
-
-# Plot the original and reconstructed sequence only per feature
-# Extract one feature for plotting
-for index in range(num_features):
-    original_feature = fixed_data_scaled[:, :, index]
-    reconstructed_feature = X_test_pred[:, :, index] 
-    plt.figure(figsize=(10, 6))
-    plt.plot(original_feature.mean(axis=1), label="Original (avg over timesteps)")
-    plt.plot(reconstructed_feature.mean(axis=1), label="Reconstructed (avg over timesteps)")
-    plt.xlabel("Sample")
-    plt.ylabel(f"Average Feature Value {combined_mapping_dict[index]}")
-    plt.title("Original vs. Reconstructed Feature")
-    plt.legend()
-    plt.show()
 
 # --- Optional: Flag Anomalies ---
 # You can set a threshold (e.g., based on percentile or statistical properties)
-threshold = np.percentile(reconstruction_errors, 95)  # 95th percentile as threshold
-anomalies = reconstruction_errors > threshold
-print(f"Number of anomalies detected: {np.sum(anomalies)} out of {len(reconstruction_errors)} samples, The current threshold is {threshold}")
+anomalies = np.zeros(fixed_data_scaled.shape[0], dtype=bool)
+for index in range(num_features):
+    feature_reconstruction_errors = np.mean(np.power(fixed_data_scaled[:, :, index] - X_test_pred[:, :, index], 2), axis=1)
+    threshold = np.percentile(feature_reconstruction_errors, 95)  # 95th percentile as threshold
+    anomalies = feature_reconstruction_errors > threshold
+    print(f"Number of anomalies detected in feature {combined_mapping_dict[index]}: {np.sum(anomalies)} out of {len(reconstruction_errors)}, The current threshold is {threshold}")
+    
+# Plot the original and reconstructed sequence only per feature
+# Create a subplot for each feature
+fig, ax = plt.subplots(num_features, 1)
+for index in range(num_features):
+    original_feature = fixed_data_scaled[:, :, index]
+    reconstructed_feature = X_test_pred[:, :, index]
+    ax[index].plot(original_feature.mean(axis=1), label="Original (avg over timesteps)")
+    ax[index].plot(reconstructed_feature.mean(axis=1), label="Reconstructed (avg over timesteps)")
+    ax[index].scatter(np.where(anomalies)[0], original_feature.mean(axis=1)[anomalies], c='red', s=10)
+    ax[index].set_xlabel("Sample")
+    ax[index].set_ylabel(f"Average Feature Value {combined_mapping_dict[index]}")
+    ax[index].legend()
+plt.show()
