59eee9c24edaeb3218b6a2d4ad53e994bda34ca1
silipwn <nambiar1@purdue.edu>
2025-05-09 08:03:30 -0400

    Remove legacy

--- ArduPilot/notify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-log=$(tail -n 7 $PGFUZZ_HOME/ArduPilot/fuzzing.log)
-curl -s -X POST -d chat_id=$CHAT_ID -d text="Fuzzer on $(hostname) exited with following: $log" "https://api.telegram.org/bot$TOKEN/sendMessage" > /dev/null
